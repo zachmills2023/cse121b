aac837c9351d9771736ded1c738dca571a350260
zachmills135 <zachmills135@gmail.com>
2023-10-24 08:20:06 -0600

    Fixed app to show data for windspeed and humidity.

--- scripts/final-project.js
+++ scripts/final-project.js
@@ -57,13 +57,29 @@ function createChart(parsedData) {
         type: 'line',
         data: {
             labels: parsedData.hourlyForecast.map(data => data.time),
-            datasets: [{
-                label: 'Temperature (°C)',
-                data: parsedData.hourlyForecast.map(data => data.temperature),
-                fill: false,
-                borderColor: 'rgb(75, 192, 192)',
-                tension: 0.1
-            }]
+            datasets: [
+                {
+                    label: 'Temperature (°C)',
+                    data: parsedData.hourlyForecast.map(data => data.temperature),
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                },
+                {
+                    label: 'Humidity (%)',
+                    data: parsedData.hourlyForecast.map(data => data.humidity),
+                    fill: false,
+                    borderColor: 'rgb(255, 99, 132)',
+                    tension: 0.1
+                },
+                {
+                    label: 'Wind Speed (m/s)',
+                    data: parsedData.hourlyForecast.map(data => data.windspeed),
+                    fill: false,
+                    borderColor: 'rgb(54, 162, 235)',
+                    tension: 0.1
+                }
+            ]
         },
     });
 }
